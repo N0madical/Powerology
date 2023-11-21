cfa414c3f006b11933c70c72f8121f3bc99e15c9
Nomadical <nomadicalyt@gmail.com>
2023-11-21 01:05:12 -0600

    v check

--- extention/powerologyWeb/index.js
+++ extention/powerologyWeb/index.js
@@ -1,5 +1,11 @@
 version = browser.runtime.getManifest().version
-document.getElementById("body").insertAdjacentHTML("afterbegin", `
-    <h4 style="color: red; text-align: center;">Please uninstall your existing version of Powerology before installing a new one</h4>
-    <h4 style="text-align: center;">Current installed version: v${version}</h4>
-`)
+setTimeout(checkVersion, 0)
+function checkVersion() {
+    console.debug(document.getElementById("body").innerHTML)
+    if(document.getElementById("body").innerHTML != ""){
+        document.getElementById("body").insertAdjacentHTML("afterbegin", `
+            <h4 style="color: red; text-align: center;">Please uninstall your existing version of Powerology before installing a new one</h4>
+            <h4 style="text-align: center;">Current installed version: v${version}</h4>
+        `)
+    }
+}
