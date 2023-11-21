e4c2771c6ea67acb68672545abe0aeaa3cc6deac
Nomadical <nomadicalyt@gmail.com>
2023-11-21 13:05:00 -0600

    version check works

--- extention/index.js
+++ extention/index.js
@@ -2,17 +2,16 @@ if(document.getElementById("app-run-5922356464") != null) {
     document.getElementById("app-run-5922356464").remove()
 }
 
-console.debug("I'm working")
-
-lastCheck = 0
-browser.storage.local.get("lastCheck").then(onGetLastCheck, onError)
-function onGetLastCheck(value) {;
-    lastCheck = value.lastCheck; 
-    if(lastCheck == undefined) {
-        lastCheck = Date.now()
-        browser.storage.local.set({lastCheck})
+nextCheck = 0
+browser.storage.local.get("nextCheck").then(onGetnextCheck, onError)
+function onGetnextCheck(value) {;
+    nextCheck = value.nextCheck; 
+    if(nextCheck == undefined) {
+        nextCheck = Date.now()
+        browser.storage.local.set({nextCheck})
     }
-    if(lastCheck <= Date.now()) {
+    console.debug("It is currently", Date.now(), "ms, app will check for update at", nextCheck, "ms.")
+    if(nextCheck <= Date.now()) {
         let gitHub = new XMLHttpRequest()
         gitHub.open("GET", "https://api.github.com/repos/N0madical/Powerology/contents/versions")
         gitHub.send()
@@ -20,12 +19,11 @@ function onGetLastCheck(value) {;
             versionlist = JSON.parse(gitHub.response)
             vname = versionlist[versionlist.length-1].name
             vnumber = vname.substring(vname.indexOf("-v")+2,vname.indexOf("-v")+5)
-            console.debug(parseFloat(vnumber), parseFloat(browser.runtime.getManifest().version))
-            if(parseFloat(vnumber) < parseFloat(browser.runtime.getManifest().version)) {
+            if(parseFloat(vnumber) > parseFloat(browser.runtime.getManifest().version)) {
                 window.open("https://powerology.aidencunningham.com/")
             }
-            lastCheck = Date.now //+ 86400000
-            browser.storage.local.set({lastCheck})
+            nextCheck = Date.now() + 86400000
+            browser.storage.local.set({nextCheck})
         }
     }
 }
